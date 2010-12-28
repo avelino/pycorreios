4eeb735d45794ae782470deeccae9c2bfcbd276e
Thiago Avelino <thiagoavelinoster@gmail.com>
2010-12-28 03:41:08 -0200

    Mudando o paradigma para orientado à objetos e adicionado setup

---
+++ pycorreios/__init__.py
@@ -0,0 +1,18 @@
+import correios
+from correios import *
+import cod
+from cod import *
+
+# __all__ = (correios.__all__ + cod.__all__)
+
+__author__ = 'Thaigo Avelino'
+
+VERSION = (0, 1, 0)
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    return version
+
+__version__ = get_version()
